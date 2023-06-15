7e3b6110b06fa06059b361fde42c52f97d7b0c5d
marouaneTalbi <talbi.918marouane@gmail.com>
2023-06-15 22:02:09 +0200

    connexion a la bdd avec sequelize

--- nodejs/App.js
+++ nodejs/App.js
@@ -1,44 +1,70 @@
-const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config({path:'./config/config.env'});
-require('./config/conn');
+const Sequelize = require('sequelize');
+const config = require('./config/conn');
 
-const app = express();
-const route = require('./routes/userRoute');
-const userModel = require("./models/userModel");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const sequelize = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    dialect: config.development.dialect
+  }
+);
 
-//Using express.json to get request of json data
-app.use(express.json());
-app.use(express.urlencoded({extended: false}))
-app.use('/api', route);
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
-app.get('/login', (req, res) => {
-    res.send(`
-    <h1>Formulaire de connexion</h1>
-    <form method="POST" action="/api/login" enctype="application/x-www-form-urlencoded">
-      <input type="email" name="email" placeholder="Adresse e-mail" required><br>
-      <input type="password" name="password" placeholder="Mot de passe" required><br>
-      <button type="submit">Se connecter</button>
-    </form>
-  `);
-});
+// Testez la connexion à la base de données
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connexion à la base de données établie avec succès.');
+  })
+  .catch((error) => {
+    console.error('Impossible de se connecter à la base de données:', error);
+  });
 
-app.get('/register', (req, res) => {
-    res.send(`
-    <h1>Formulaire d'inscription</h1>
-    <form method="POST" action="/api/login" enctype="application/json">
-      <input type="text" name="nom" placeholder="Nom" required><br>
-      <input type="email" name="email" placeholder="Adresse e-mail" required><br>
-      <input type="password" name="password" placeholder="Mot de passe" required><br>
-      <button type="submit">S'inscrire</button>
-    </form>
-  `);
-});
-app.listen(process.env.PORT, () => {
-    console.log('Serveur Express en cours d\'exécution sur le port 3000');
-});
+
+
+// const express = require('express');
+// const dotenv = require('dotenv');
+// dotenv.config({path:'./config/config.env'});
+// require('./config/conn');
+
+// const app = express();
+// const route = require('./routes/userRoute');
+// const userModel = require("./models/userModel");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+
+// //Using express.json to get request of json data
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}))
+// app.use('/api', route);
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
+
+// app.get('/login', (req, res) => {
+//     res.send(`
+//     <h1>Formulaire de connexion</h1>
+//     <form method="POST" action="/api/login" enctype="application/x-www-form-urlencoded">
+//       <input type="email" name="email" placeholder="Adresse e-mail" required><br>
+//       <input type="password" name="password" placeholder="Mot de passe" required><br>
+//       <button type="submit">Se connecter</button>
+//     </form>
+//   `);
+// });
+
+// app.get('/register', (req, res) => {
+//     res.send(`
+//     <h1>Formulaire d'inscription</h1>
+//     <form method="POST" action="/api/login" enctype="application/json">
+//       <input type="text" name="nom" placeholder="Nom" required><br>
+//       <input type="email" name="email" placeholder="Adresse e-mail" required><br>
+//       <input type="password" name="password" placeholder="Mot de passe" required><br>
+//       <button type="submit">S'inscrire</button>
+//     </form>
+//   `);
+// });
+// app.listen(process.env.PORT, () => {
+//     console.log('Serveur Express en cours d\'exécution sur le port 3000');
+// });
--- nodejs/config/conn.js
+++ nodejs/config/conn.js
@@ -1,20 +1,46 @@
-const { Pool } = require('pg');
+module.exports = {
+    development: {
+        host: 'localhost',
+        port: 5432,
+        database: 'mydatabase',
+        username: 'myuser',
+        password: 'mypassword',
+        dialect: 'postgres'
+    },
+  };
 
-const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'ChallengeStack',
-    user: 'root',
-    password: 'password1234'
-});
 
-pool.connect((err, client, done) => {
-    if (err) {
-        console.error('Erreur de connexion à la base de données :', err);
-    } else {
-        console.log('Connecté à la base de données PostgreSQL');
-        done();
-    }
-});
+//   module.exports = {
+//     development: {
+//       database: 'your_database',
+//       username: 'your_username',
+//       password: 'your_password',
+//       host: 'localhost',
+//       port: 27017,
+//       dialect: 'mongodb'
+//     },
+//     production: {
+//       // Configurations pour l'environnement de production
+//     }
+//   };
 
-module.exports = pool;
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'mydatabase',
+//     user: 'myuser',
+//     password: 'mypassword'
+// });
+
+// pool.connect((err, client, done) => {
+//     if (err) {
+//         console.error('Erreur de connexion à la base de données :', err);
+//     } else {
+//         console.log('Connecté à la base de données PostgreSQL');
+//         done();
+//     }
+// });
+
+// module.exports = pool;
