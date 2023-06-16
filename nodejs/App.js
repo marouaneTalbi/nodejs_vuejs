74a034dc57b1a43088d1ab2fb369bc45c792eac6
marouaneTalbi <talbi.918marouane@gmail.com>
2023-06-17 01:15:15 +0200

    ajouter les formulaire login et register sur vuejs et ajouter les headers coté servuer pour pouvoir comuniquer avec le serveur

--- nodejs/App.js
+++ nodejs/App.js
@@ -1,13 +1,13 @@
 
 const express = require('express');
-// const dotenv = require('dotenv');
-// dotenv.config({path:'./config/config.env'});
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
 const app = express();
 const route = require('./routes/userRoute');
 const userModel = require("./models/userModel");
 const sequelize = require('./config/conn');
+const helmet = require('helmet');
+const cors = require('cors');
+app.use(cors());
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
@@ -16,29 +16,17 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.get('/login', (req, res) => {
-    res.send(`
-    <h1>Formulaire de connexion</h1>
-    <form method="POST" action="/login" enctype="application/x-www-form-urlencoded">
-      <input type="mail" name="mail" placeholder="Adresse e-mail" required><br>
-      <input type="password" name="password" placeholder="Mot de passe" required><br>
-      <button type="submit">Se connecter</button>
-    </form>
-  `);
-});
 
-app.get('/register', (req, res) => {
-    res.send(`
-    <h1>Formulaire d'inscription</h1>
-    <form method="POST" action="/register" enctype="application/json">
-      <input type="text" name="pseudo" placeholder="pseudo" required><br>
-      <input type="mail" name="mail" placeholder="Adresse e-mail" required><br>
-      <input type="password" name="password" placeholder="Mot de passe" required><br>
-      <button type="submit">S'inscrire</button>
-    </form>
-  `);
-});
+helmet.contentSecurityPolicy({
+  connectSrc: ["'self'", 'http://localhost:5173/'],
+})
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin','*')
+  res.setHeader('Cross-Origin-Resource-Policy','*')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.listen('3000', () => {
     console.log('Serveur Express en cours d\'exécution sur le port 3000');
