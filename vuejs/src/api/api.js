b1d7c89149a7dc51abef11d1b4252c13a7e4b3e7
TheoBourdel <bourdeltheo@gmail.com>
2023-06-26 21:02:43 +0200

    add patch + delete function for axios

--- vuejs/src/api/api.js
+++ vuejs/src/api/api.js
@@ -10,3 +10,10 @@ export function postData(url, data) {
     return axios.post(url, data);
 }
 
+export function deleteData(route, data) {
+    return axios.delete(serverURI + route + data);
+}
+
+export function patchData(route, data) {
+    return axios.patch(serverURI + route, data);
+}
