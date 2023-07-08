bae22a656d8ccabe5d0da2554ac1c4ce810bf173
marouaneTalbi <79101316+marouaneTalbi@users.noreply.github.com>
2023-07-08 23:13:31 +0200

    Revert "Skins"

--- vuejs/src/views/HomeView.vue
+++ vuejs/src/views/HomeView.vue
@@ -1,28 +1,9 @@
-<template>
-  <header id="headerHome">
-    <span>LOGO</span>
-    <div>
-      <a href="/login" id="buttonLogin">Connexion</a>
-      <a href="/register" id="buttonRegister">Inscription</a>
-    </div>
-  </header>
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
 
+<template>
   <main>
-    <section id="homePresentation">
-      <h1>Bienvenue dans le monde du Défi de Mémoire !</h1>
-      <p>Le jeu de mémoire ultime vous attend, prêt à mettre vos capacités de concentration et de rappel à l'épreuve</p>
-      <a href="/login">Jouer</a>
-    </section>
-    <div class="des-container-home bg-img-container">
-    </div>
-    <div class="crayon-container-home bg-img-container">
-    </div>
-    <div class="dollars-container-home bg-img-container">
-    </div>
+    <TheWelcome />
   </main>
 </template>
-
-<script setup>
-
-import './../styles/home.css';
-</script>
