8629836f9d9dcf5a617facc76d1ad27f61094e75
TheoBourdel <bourdeltheo@gmail.com>
2023-07-11 13:51:21 +0200

    clean app js

--- nodejs/App.js
+++ nodejs/App.js
@@ -2,34 +2,32 @@ const express = require('express');
 const app = express();
 const route = require('./routes/userRoute');
 const skinRoute = require('./routes/skinRoute');
-
 const User = require("./controllers/UserController")
-
-const sequelize = require('./config/conn');
 const helmet = require('helmet');
 const cors = require('cors');
-
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const path = require('path');
-
 const gameSocket = require('./sockets/gameSocket');
 
 // SOCKET.IO //
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-      origins: ['http://localhost:8080']
+        origins: ['http://localhost:8080']
     }
-  });
+});
+io.on('connection', (socket) => {
+    gameSocket(socket);
+});
 // SOCKET.IO //
 
 
-// MONGODB CONNECTION //
+// MONGODB connection
 const mongodb = require('./db/mongo');
 mongodb.initClientDbConnection();
-// MONGODB CONNECTION //
 
+// MIDDLEWARE 
+app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
 app.use(session({
@@ -42,37 +40,17 @@ app.use(session({
         maxAge: 7 * 24 * 60 * 60 * 1000
     }
 }));
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
+// ROUTES
 app.use('/', route);
-
-const corsOptions = {
-    origin: 'http://localhost:8080/pictures',
-};
-
-app.use(cors(corsOptions));
-app.use('/pictures', cors({
-    origin: '*'
-}), express.static(path.join(__dirname, 'pictures')));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});
-
-
 app.use('/', skinRoute);
-
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-
-
+// à mettre dans un autre fichier
 app.get('/confirm', async (req, res) => {
     const token = req.query.token;
     try {
@@ -85,22 +63,22 @@ app.get('/confirm', async (req, res) => {
     }
 });
 
+// CSP configuration
 helmet.contentSecurityPolicy({
-  connectSrc: ["'self'", 'http://localhost:5173/'],
+    connectSrc: ["'self'", 'http://localhost:5173/'],
 })
 
+// CORS configuration
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin','*')
-  res.setHeader('Cross-Origin-Resource-Policy','*')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  next();
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Cross-Origin-Resource-Policy','*')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    next();
 });
 
+// START express server
 server.listen('3000', () => {
     console.log('Serveur Express en cours d\'exécution sur le port 3000');
 });
 
-io.on('connection', (socket) => {
-    gameSocket(socket);
-});
