089cff9864449ea092c5d7828e757d5194c00cf1
TheoBourdel <bourdeltheo@gmail.com>
2023-07-11 16:05:31 +0200

    user test

--- nodejs/tests/unitaire/user.test.js
+++ nodejs/tests/unitaire/user.test.js
@@ -1,23 +1,6 @@
-const { getUsers } = require('../../controllers/AdminController');
+const { getUsers, getUser } = require('../../controllers/AdminController');
 const UserMongo = require('../../models/userModelMongo');
 const request = require("supertest"); // Importez supertest correctement
-
-describe('getUser', () => {
-    it('returns a 404 error', async() => {
-        const userId = 999999999;
-
-        await request('http://localhost:3000')
-        .get(`/user/${userId}`)
-        .expect(404);    
-    })
-    it('return user with 200', async() => {
-        const userId = 20;
-
-        await request('http://localhost:3000')
-        .get(`/user/${userId}`)
-        .expect(200);    
-    })
-})
 
 describe('getUsers', () => {
     it('return users list', async() => {
@@ -35,6 +18,26 @@ describe('getUsers', () => {
         };
 
         await getUsers({}, mockResponse);
+
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+        expect(mockResponse.json).toHaveBeenCalledWith(mockUsers);
+    })
+})
+
+describe('getUser', () => {
+    it('return user', async() => {
+        const mockUser = [
+            { name: 'User 1' },
+        ];
+
+        UserMongo.find = jest.fn().mockResolvedValue(mockUser);
+
+        const mockResponse = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        await getUser({}, mockResponse);
 
         expect(mockResponse.status).toHaveBeenCalledWith(200);
         expect(mockResponse.json).toHaveBeenCalledWith(mockUsers);
