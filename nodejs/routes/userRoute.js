134f95c3a6d53fdecfe2a9c5b3f0edcaea5f0c3c
unknown <aminecheriguifr@gmail.com>
2023-07-24 15:18:32 +0200

    fix routes bug

--- nodejs/routes/userRoute.js
+++ nodejs/routes/userRoute.js
@@ -15,20 +15,20 @@ route.post('/initPassword',  UserController.initPassword);
 route.put('/user/:id/verify-email', UserController.updateIsConfirmed);
 
 // Routes protected by authentication middleware
-route.use(authMiddleware());
-route.get('/user/skins/:id', UserController.getUserSkins);
-route.get('/user/skin/:id', UserController.getUserSkin);
-route.post('/logout',  UserController.logout);
-route.put("/user/:id/updateuser", UserController.updateUser);
-route.put('/user/:id/change-password', UserController.changePassword);
+const gamerAuthMiddleware = authMiddleware();
+route.get('/user/skins/:id',gamerAuthMiddleware, UserController.getUserSkins);
+route.get('/user/skin/:id',gamerAuthMiddleware, UserController.getUserSkin);
+route.post('/logout',gamerAuthMiddleware,  UserController.logout);
+route.put("/user/:id/updateuser",gamerAuthMiddleware, UserController.updateUser);
+route.put('/user/:id/change-password',gamerAuthMiddleware, UserController.changePassword);
 
 
 // Routes protected by authentication and role Admin
-route.use(authMiddleware(['admin']));
-route.get('/user/:id', AdminController.getUser)
-route.get('/users', AdminController.getUsers)
-route.delete('/user/:id', AdminController.deleteUser)
-route.patch('/user/:id', AdminController.updateUser)
+const adminAuthMiddleware = authMiddleware(['admin']);
+route.get('/user/:id', adminAuthMiddleware, AdminController.getUser)
+route.get('/users',adminAuthMiddleware,  AdminController.getUsers)
+route.delete('/user/:id', adminAuthMiddleware, AdminController.deleteUser)
+route.patch('/user/:id', adminAuthMiddleware, AdminController.updateUser)
 
 module.exports = route;
 
