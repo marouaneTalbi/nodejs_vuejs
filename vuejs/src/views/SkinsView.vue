16838655ccf2e623826223187f3f371f9332c2f1
marouaneTalbi <talbi.918marouane@gmail.com>
2023-07-25 17:21:49 +0200

    conflis reglé

--- vuejs/src/views/SkinsView.vue
+++ vuejs/src/views/SkinsView.vue
@@ -2,27 +2,12 @@
   <section class="skins">
     <Header />
 
-<<<<<<< HEAD
-          <div class="form-group">
-            <label for="money_type">Type de monnaie</label>
-            <input type="text" style="color:white" v-model="money_type" id="money_type" placeholder="Type de monnaie" required>
-          </div>
-
-          <div class="form-group">
-            <label for="money_type">Coins</label>
-            <input type="text" style="color:white" v-model="coins_price" id="coins_price" placeholder="Coins" required>
-            <p v-if="!isValidPrice" class="error-message">Veuillez entrer une valeur numérique pour les Coins.</p>
-          </div>
-          <button type="submit">Créer</button>
-        </form>
-=======
     <Modal @close="toggleModal" @confirm="handleConfirm" :modalActive="modalActive" :showConfirmButton="false">
       <div class="modal-content">
         <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 13V5a2 2 0 0 0-2-2h-2m0 0H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h7M13 3v10.5M9 7v3"></path><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 22v-7.1a7 7 0 0 0-2.052-4.95L16.998 8v6.587c0 .89-1.077 1.337-1.707.707L13.996 14c-.5-.5-1.701-.8-2.502 0-.8.8-.5 2 0 2.5l5.503 5.5"></path></svg>
         <h2>Paiement</h2>
         <button class="buy-button" @click="buySkin()">Payer par carte</button>
         <button class="buy-button buy-button--coins" @click="buyByCoins()">Payer avec Les Coins</button>
->>>>>>> develop
       </div>
     </Modal>
 
