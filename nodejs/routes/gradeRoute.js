18d9fa51e150533548d22e9dc78cb10a167470ac
marouaneTalbi <talbi.918marouane@gmail.com>
2023-07-25 14:01:41 +0200

    debugger version prod

--- nodejs/routes/gradeRoute.js
+++ nodejs/routes/gradeRoute.js
@@ -3,14 +3,14 @@ const gradeRoute = express.Router();
 const GradeController = require('../controllers/grade/GradeController');
 const authMiddleware = require("../middlewares/authMiddleware");
 
-// const adminAuthMiddleware = authMiddleware(['admin']);
-// gradeRoute.post('/grade/create', adminAuthMiddleware, GradeController.createGrade)
-// gradeRoute.patch('/grade/update/:id', adminAuthMiddleware, GradeController.updateGrade)
-// gradeRoute.delete('/grade/delete/:id', adminAuthMiddleware, GradeController.deleteGrade)
+const adminAuthMiddleware = authMiddleware(['admin']);
+gradeRoute.post('/grade/create', adminAuthMiddleware, GradeController.createGrade)
+gradeRoute.patch('/grade/update/:id', adminAuthMiddleware, GradeController.updateGrade)
+gradeRoute.delete('/grade/delete/:id', adminAuthMiddleware, GradeController.deleteGrade)
 
-// // const bothAuthMiddleware = authMiddleware(['admin','gamer']);
-// gradeRoute.get('/grades',  bothAuthMiddleware, GradeController.getAllGrades)
-// gradeRoute.get('/grade/:id', bothAuthMiddleware, GradeController.getGradeByUserId)
-// gradeRoute.get('/top-gamers',bothAuthMiddleware,  GradeController.getTopGamers)
+const bothAuthMiddleware = authMiddleware(['admin','gamer']);
+gradeRoute.get('/grades',  bothAuthMiddleware, GradeController.getAllGrades)
+gradeRoute.get('/grade/:id', bothAuthMiddleware, GradeController.getGradeByUserId)
+gradeRoute.get('/top-gamers',bothAuthMiddleware,  GradeController.getTopGamers)
 
 module.exports = gradeRoute;
--- nodejs/routes/userRoute.js
+++ nodejs/routes/userRoute.js
@@ -17,17 +17,17 @@ route.get('/user/:id/stats', UserController.getUserStats);
 route.get('/user/:id/games-history', UserController.getUserGamesHistory);
 
 // Routes protected by authentication middleware
-// const gamerAuthMiddleware = authMiddleware();
-// route.get('/user/skins/:id',gamerAuthMiddleware, UserController.getUserSkins);
-// route.get('/user/skin/:id',gamerAuthMiddleware, UserController.getUserSkin);
-// route.post('/logout',gamerAuthMiddleware,  UserController.logout);
-// route.put("/user/:id/updateuser",gamerAuthMiddleware, UserController.updateUser);
-// route.put('/user/:id/change-password',gamerAuthMiddleware, UserController.changePassword);
+const gamerAuthMiddleware = authMiddleware();
+route.get('/user/skins/:id',gamerAuthMiddleware, UserController.getUserSkins);
+route.get('/user/skin/:id',gamerAuthMiddleware, UserController.getUserSkin);
+route.post('/logout',gamerAuthMiddleware,  UserController.logout);
+route.put("/user/:id/updateuser",gamerAuthMiddleware, UserController.updateUser);
+route.put('/user/:id/change-password',gamerAuthMiddleware, UserController.changePassword);
 
 
 // Routes protected by authentication and role Admin
 
-// route.use(authMiddleware(['admin']));
+route.use(authMiddleware(['admin']));
 route.get('/user/:id', AdminController.getUser)
 route.get('/users', AdminController.getUsers)
 route.delete('/user/:id', AdminController.deleteUser)
