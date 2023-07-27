8e8c3564d02a58378ffe273b746ee2edb31745ee
marouaneTalbi <talbi.918marouane@gmail.com>
2023-07-27 12:40:58 +0200

    ajouter les variable d'environnement

--- vuejs/src/api/api.js
+++ vuejs/src/api/api.js
@@ -1,11 +1,8 @@
 import axios from 'axios';    
 import Cookies from 'js-cookie';
 
-// prod
-// export const serverURI = 'https://challenge.ovh:3000';
 
-export const serverURI = 'http://localhost:3000';
-
+export const serverURI = import.meta.env.VITE_API_URL_SERVER_URL;
 
 axios.interceptors.request.use((config) => {
     const token = Cookies.get('token');
