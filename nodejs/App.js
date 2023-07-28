dd70729167e6df5040f6c1cc0f0c2fe0232feae6
marouaneTalbi <talbi.918marouane@gmail.com>
2023-07-28 13:53:10 +0200

    secu app.js

--- nodejs/App.js
+++ nodejs/App.js
@@ -43,14 +43,14 @@ mongodb.initClientDbConnection();
 // MONGODB CONNECTION //
 
 // SOCKET.IO //
- const server = require('http').createServer(app);
+//  const server = require('http').createServer(app);
 
-// const options = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/challenge.ovh/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/challenge.ovh/fullchain.pem'),
-// };
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/challenge.ovh/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/challenge.ovh/fullchain.pem'),
+};
 
-// const server = https.createServer(options, app);
+const server = https.createServer(options, app);
 
 
 
